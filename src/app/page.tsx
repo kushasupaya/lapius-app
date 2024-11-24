a825a1a4fe312cca2fc927d37607b8c1a28276cd
Anish Pradhananga <anish.pradhananga@gmail.com>
2024-11-24 10:52:44 +0545

    minor adjustment to hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,31 @@ export default function Home() {
   return (
     <>
       <div className="bg-secondary w-full">
-        <div className="bg-[url('/images/pattern.svg')] h-80 w-full bg-no-repeat bg-center"></div>
         <div className="container mx-auto px-4 md:px-0">
-          <div className="-mt-32 pb-12">
-            <h1 className="text-white text-[40px] font-semibold max-w-[600px]">
-              Take Control of Your Healthcare Costs with the AI
-              <span 
-                className="bg-[url('/icons/ai.svg')] bg-contain bg-no-repeat inline-block h-8 w-8 ml-3"
-                aria-hidden="true"
+          <div className="flex flex-col justify-between md:min-h-screen">
+            <div>
+              <div className="bg-[url('/images/pattern.svg')] h-80 w-full bg-no-repeat bg-center"></div>
+              <div className="-mt-32 pb-6">
+                <h1 className="text-white text-[40px] font-semibold max-w-[600px]">
+                  Take Control of Your Healthcare Costs with the AI
+                  <span 
+                    className="bg-[url('/icons/ai.svg')] bg-contain bg-no-repeat inline-block h-8 w-8 ml-3"
+                    aria-hidden="true"
+                  />
+                </h1>
+                <p className="mt-2 max-w-[550px] text-subtitle-white">Transform your social media strategy with our all-in-one platform designed to help you plan, create, and analyze your content effortlessly.</p>
+                <Button variant="primary" size="primary" className="mt-6">Get Started</Button>
+              </div>
+            </div>
+            <div className="hidden md:block overflow-hidden h-[350px] w-full rounded-t-2xl">
+              <Image
+                src="/images/hero.jpeg"
+                alt="Lapius AI"
+                width={1200}
+                height={1200}
+                className="object-cover w-full h-auto"
               />
-            </h1>
-            <p className="mt-2 max-w-[550px] text-subtitle-white">Transform your social media strategy with our all-in-one platform designed to help you plan, create, and analyze your content effortlessly.</p>
-            <Button variant="primary" size="primary" className="mt-6">Get Started</Button>
-          </div>
-          <div className="hidden md:block overflow-hidden h-[350px] w-full rounded-t-2xl shadow-lg shadow-secondary">
-            <Image
-              src="/images/hero.jpeg"
-              alt="Lapius AI"
-              width={1200}
-              height={1200}
-              className="object-cover w-full h-auto"
-            />
+            </div>
           </div>
         </div>
       </div>
