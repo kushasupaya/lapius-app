2fda8027436685c6779f25818f642c19133343d4
kushasupaya <kushas.upaya@gmail.com>
2024-11-26 01:14:06 -0500

    package added and small routing fix for headers

--- src/components/sections/home/hero-section.tsx
+++ src/components/sections/home/hero-section.tsx
@@ -2,37 +2,42 @@ import Image from "next/image";
 import { GetStartedButton } from "./components";
 
 const HeroSection = () => {
-  return (
-    <div className="bg-secondary w-full">
-      <div className="container mx-auto px-4 md:px-0">
-        <div className="flex flex-col justify-between md:min-h-screen">
-          <div>
-            <div className="bg-[url('/images/pattern.svg')] h-80 w-full bg-no-repeat bg-center"></div>
-            <div className="-mt-32 pb-6">
-              <h1 className="text-white text-[40px] font-semibold max-w-[600px]">
-                Take Control of Your Healthcare Costs with the AI
-                <span 
-                  className="bg-[url('/icons/ai.svg')] bg-contain bg-no-repeat inline-block h-8 w-8 ml-3"
-                  aria-hidden="true"
-                />
-              </h1>
-              <p className="mt-2 max-w-[550px] text-subtitle-white">Transform your social media strategy with our all-in-one platform designed to help you plan, create, and analyze your content effortlessly.</p>
-              <GetStartedButton />
+    return (
+        <div className="bg-secondary w-full">
+            <div className="container mx-auto px-4 md:px-0">
+                <div className="flex flex-col justify-between md:min-h-screen">
+                    <div>
+                        <div className="bg-[url('/images/pattern.svg')] h-80 w-full bg-no-repeat bg-center"></div>
+                        <div className="-mt-32 pb-6">
+                            <h1 className="text-white text-[40px] font-semibold max-w-[600px]   leading-tight">
+                                Take Control of Your Healthcare Costs with the
+                                AI
+                                <span
+                                    className="bg-[url('/icons/ai.svg')] bg-contain bg-no-repeat inline-block h-8 w-8 ml-3"
+                                    aria-hidden="true"
+                                />
+                            </h1>
+                            <p className="mt-2 max-w-[550px] text-subtitle-white">
+                                Transform your social media strategy with our
+                                all-in-one platform designed to help you plan,
+                                create, and analyze your content effortlessly.
+                            </p>
+                            <GetStartedButton />
+                        </div>
+                    </div>
+                    <div className="hidden md:block overflow-hidden h-[350px] w-full rounded-t-2xl">
+                        <Image
+                            src="/images/hero.jpeg"
+                            alt="Lapius AI"
+                            width={1200}
+                            height={1200}
+                            className="object-cover w-full h-auto"
+                        />
+                    </div>
+                </div>
             </div>
-          </div>
-          <div className="hidden md:block overflow-hidden h-[350px] w-full rounded-t-2xl">
-            <Image
-              src="/images/hero.jpeg"
-              alt="Lapius AI"
-              width={1200}
-              height={1200}
-              className="object-cover w-full h-auto"
-            />
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default HeroSection;
--- src/components/sections/home/services-section.tsx
+++ src/components/sections/home/services-section.tsx
@@ -2,16 +2,33 @@ import { SectionTitle, StatsCard } from "./components";
 
 const ServicesSection = () => {
   return (
-    <div className="bg-white">
+    <div id="about" className="bg-white">
       <div className="container mx-auto p-4 lg:px-0 md:pt-20 md:pb-5">
         <div className="flex flex-col items-center w-full mb-10">
           <SectionTitle title="About Lapius" />
-          <h2 className="max-w-[607px] mt-2.5 text-black text-[40px] font-semibold text-center">Excepteur sint occaecat cupidatat non proident </h2>
+          <h2 className="max-w-[607px] mt-2.5 text-black text-[40px] font-semibold text-center leading-tight">
+            Excepteur sint occaecat cupidatat non proident{" "}
+          </h2>
         </div>
         <div className="flex flex-col justify-center items-center lg:flex-row gap-6">
-          <StatsCard icon="/icons/home.svg" stats="6,000+" title="Hospitals mapped" description="Sit a cursus aliquam sed leo amet nulla at sed nisl at semper ultricies at mattis sit a dignissim amet pretium vitae nunc." />
-          <StatsCard icon="/icons/file.svg" stats="12.2%" title="Dispute their bills" description="Sit a cursus aliquam sed leo amet nulla at sed nisl at semper ultricies at mattis sit a dignissim amet pretium vitae nunc." />
-          <StatsCard icon="/icons/warning.svg" stats="80%" title="Medical bills contain errors" description="Sit a cursus aliquam sed leo amet nulla at sed nisl at semper ultricies at mattis sit a dignissim amet pretium vitae nunc." />
+          <StatsCard
+            icon="/icons/home.svg"
+            stats="6,000+"
+            title="Hospitals mapped"
+            description="Sit a cursus aliquam sed leo amet nulla at sed nisl at semper ultricies at mattis sit a dignissim amet pretium vitae nunc."
+          />
+          <StatsCard
+            icon="/icons/file.svg"
+            stats="12.2%"
+            title="Dispute their bills"
+            description="Sit a cursus aliquam sed leo amet nulla at sed nisl at semper ultricies at mattis sit a dignissim amet pretium vitae nunc."
+          />
+          <StatsCard
+            icon="/icons/warning.svg"
+            stats="80%"
+            title="Medical bills contain errors"
+            description="Sit a cursus aliquam sed leo amet nulla at sed nisl at semper ultricies at mattis sit a dignissim amet pretium vitae nunc."
+          />
         </div>
       </div>
     </div>
