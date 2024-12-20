cf072315c3e49992b0c363ebae3a3c83bb47e02e
kushasupaya <kushas.upaya@gmail.com>
2024-12-20 17:19:29 -0500

    added icons for social media

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,5 +1,11 @@
 import Link from "next/link";
 import Logo from "./logo";
+import {
+  IconBrandFacebook,
+  IconBrandInstagram,
+  IconBrandLinkedin,
+  IconBrandX,
+} from "@tabler/icons-react";
 
 const Footer = () => {
   return (
@@ -16,10 +22,11 @@ const Footer = () => {
             {/* <div className="w-[208px] mt-4.5 py-4 px-6 rounded-[48px] border border-[#292E3B] text-[#CBCBCC] text-sm text-center">
               Copyright © {new Date().getFullYear()} Lapius
             </div> */}
+            <div className="w-[208px] py-4 px-6 rounded-[48px] border border-[#292E3B] text-[#CBCBCC] text-sm text-center">
+              Copyright © {new Date().getFullYear()} Lapius
+            </div>
           </div>
-          <div className="w-[208px] py-4 px-6 rounded-[48px] border border-[#292E3B] text-[#CBCBCC] text-sm text-center">
-            Copyright © {new Date().getFullYear()} Lapius
-          </div>
+
           {/* <div className="flex flex-col gap-6 md:flex-row md:gap-20 py-2">
             <div className="flex flex-col gap-6">
               <p className="text-white text-sm font-bold">Company</p>
@@ -66,34 +73,39 @@ const Footer = () => {
                 Become an Affiliate
               </Link>
             </div>
-            <div className="flex flex-col gap-6">
-              <p className="text-white text-sm font-bold">Social</p>
-              <Link
-                href="#"
-                className="text-tertiary-foreground hover:text-primary text-sm"
-              >
-                Facebook
-              </Link>
-              <Link
-                href="#"
-                className="text-tertiary-foreground hover:text-primary text-sm"
-              >
-                Instagram
-              </Link>
-              <Link
-                href="#"
-                className="text-tertiary-foreground hover:text-primary text-sm"
-              >
-                LinkedIn
-              </Link>
-              <Link
-                href="#"
-                className="text-tertiary-foreground hover:text-primary text-sm"
-              >
-                X
-              </Link>
-            </div>
+            
           </div> */}
+          <div className="flex  gap-6">
+            <Link
+              href="https://www.facebook.com/profile.php?id=61569863414116"
+              className="text-tertiary-foreground hover:text-primary text-sm"
+              target="_blank"
+            >
+              <IconBrandFacebook className="w-6 h-6" />
+            </Link>
+            <Link
+              href="https://www.instagram.com/lapiusai/"
+              className="text-tertiary-foreground hover:text-primary text-sm"
+              target="_blank"
+            >
+              <IconBrandInstagram className="w-6 h-6" />
+            </Link>
+
+            <Link
+              href="https://x.com/LapiusAI"
+              className="text-tertiary-foreground hover:text-primary text-sm"
+              target="_blank"
+            >
+              <IconBrandX className="w-6 h-6" />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/company/lapius-ai"
+              className="text-tertiary-foreground hover:text-primary text-sm"
+              target="_blank"
+            >
+              <IconBrandLinkedin className="w-6 h-6" />
+            </Link>
+          </div>
         </div>
       </div>
       {/* <div className="bg-[url('/images/footer.png')] bg-no-repeat bg-cover h-[280px] w-full"></div> */}
