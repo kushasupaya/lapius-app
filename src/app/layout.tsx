98620c7b2760ab157c87443dc1202392c7da43fe
kushasupaya <kushas.upaya@gmail.com>
2024-12-20 20:09:25 -0500

    made some seo changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,30 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Lapius",
   description: "Take Control of Your Healthcare Costs with the AI",
+  openGraph: {
+    title: "Lapius",
+    description: "Take Control of Your Healthcare Costs with the AI",
+    type: "website",
+    url: "https://lapiusai.com",
+    images: [
+      {
+        url: "https://lapiusai.com/logo/lapius.svg",
+        width: 1200,
+        height: 1200,
+        alt: "Lapius logo",
+      },
+    ],
+  },
+  keywords: [
+    "medical billing",
+    "healthcare",
+    "AI",
+    "how to dispute a medical bill",
+    "Medical bill",
+    "hospital bill",
+    "CPT codes",
+    "medical bill mistakes",
+  ],
 };
 
 export default function RootLayout({
