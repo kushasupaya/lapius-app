792c6722dbe327fc9be7e4bfb6d244231b75c691
kushasupaya <kushas.upaya@gmail.com>
2025-03-17 16:32:55 +0100

    new module for pdf review

---
+++ src/types/filepond-plugin-pdf-preview.d.ts
@@ -0,0 +1,6 @@
+declare module "filepond-plugin-pdf-preview" {
+  import { FilePondPlugin } from "filepond";
+
+  const FilePondPluginPdfPreview: FilePondPlugin;
+  export default FilePondPluginPdfPreview;
+}
